e965d2979d3ff58c6c7bf4285a3de10878fbb833
Soe Ya Htet <shakusoe@gmail.com>
2022-11-29 22:46:07 +0800

    Web - Create CSVUpload Component.

---
+++ CSVTest/src/app/csvupload/csvupload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-csvupload',
+  templateUrl: './csvupload.component.html',
+  styleUrls: ['./csvupload.component.css']
+})
+export class CSVUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
